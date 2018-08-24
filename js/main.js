4bda77c9b8d28c876d3432c2cf844d2ce75f99f2
tadoken1989 <tadoken1989@gmail.com>
2018-08-23 23:43:00 -0700

    no message

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Slider main
+$(document).ready(function() {
+
+  $('.nav li').click(function() {
+
+    /*if($('.nav li').hasclass('active'))
+    {
+      $('.nav li').removeClass('active');
+      $(this).addClass('active');
+    },
+    else{
+      $('.nav li').addClass('active');
+    };*/
+    $('.nav li').removeClass('active');
+      $(this).addClass('active');
+    
+  });
+
+});
